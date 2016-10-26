627f03e6c65f488a8c2846371926b9379b2b3101
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-25 17:55:28 -0700

    Added .html pages to templates s and routes to server.py

--- server.py
+++ server.py
@@ -2,10 +2,11 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import (Flask, render_template, redirect, request, flash, 
+    session, jsonify)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import (connect_to_db, db, User, Rating, Movie)
 
 
 app = Flask(__name__)
@@ -22,9 +23,17 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
 
+    return render_template("homepage.html")
+    # a = jsonify([1,3])
+    # return a
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
@@ -38,4 +47,5 @@ if __name__ == "__main__":
 
 
     
-    app.run(port=5003)
+    # app.run(port=5003)
+    app.run(port=5000)
