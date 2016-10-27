72f3c5bbb7196f8cc962e0fcf8b0db9ad1e722bc
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-26 17:59:21 -0700

    Finished up until logout of part 2

--- server.py
+++ server.py
@@ -19,6 +19,7 @@ app.secret_key = "ABC"
 # error.
 app.jinja_env.undefined = StrictUndefined
 
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 @app.route('/')
 def index():
@@ -34,6 +35,38 @@ def user_list():
 
     users = User.query.all()
     return render_template("user_list.html", users=users)
+
+
+@app.route("/register", methods=["GET"])
+def register_form():
+    """Users register here"""
+
+    return render_template("register_form.html")
+
+
+@app.route("/register", methods=["POST"])
+def register_process():
+    """Checks to see that if user already exists"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # if User.query.filter(User.email == email) != None:
+    raw_emails = db.session.query(User.email).all()
+    emails = [data[0] for data in raw_emails]
+
+    if email not in emails:
+        user = User(email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash('You are logged in!')
+
+    return redirect("/")
+
+@app.route("/logout")
+def logout_user():
+    """Log out the user"""
+    flash('You are logged out')
+    return redirect("/")
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
