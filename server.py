1de2363901a79061f1fc315d392f88c2413eb146
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-27 17:52:32 -0700

    Completed User Details

--- server.py
+++ server.py
@@ -50,23 +50,45 @@ def register_process():
     email = request.form.get("email")
     password = request.form.get("password")
 
-    # if User.query.filter(User.email == email) != None:
     raw_emails = db.session.query(User.email).all()
     emails = [data[0] for data in raw_emails]
-
+# Try getting one user instead of checking whole list
     if email not in emails:
         user = User(email=email, password=password)
         db.session.add(user)
         db.session.commit()
+        session["user_id"] = user.user_id
+        print session
         flash('You are logged in!')
 
     return redirect("/")
-
+# Should have separate route for loggin in existing users
 @app.route("/logout")
 def logout_user():
     """Log out the user"""
+    session.clear() # Or take out value only
     flash('You are logged out')
     return redirect("/")
+
+@app.route("/users/<user_id>") #Route syntax in python
+def user_page(user_id):
+    
+    user = db.session.query(User).filter_by(user_id = user_id).one()
+
+    # ratings = user.ratings
+    # scores = []
+    # scored_movies = []
+    # for rating in ratings:
+    #     scores.append(rating.score)
+    #     scored_movies.append(rating.movie_id)
+    # movie_objects = []
+    # for movie in scored_movies:
+    #     movie_objects.append(db.session.query(Movie).filter_by(movie_id = movie).one())
+    # movies = []
+    # for movie in movie_objects:
+    #     movies.append(movie.title)
+
+    return render_template("user.html", user=user)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
